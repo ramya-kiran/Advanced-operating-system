e7dabb448cef9bbacafb33f8073a83f5d3f4edd9
Ramya Rao <ramrao@umail.iu.edu>
2017-09-08 21:05:17 -0400

    commented assignment 2 code

--- apps/process_ring.c
+++ apps/process_ring.c
@@ -5,15 +5,19 @@
 #include <string.h>
 
 
-
+// Function to implement message passing 
 void message_process(pid32 *next_pid, int curr, pid32 *prev_pid, int32 count_val, int32 pro_cre){                                     
   int32 last = 0;                                                                                                                      
-  while (last < count_val){                                                                                                            
+  while (last < count_val){   // checking if the number of rounds have been completed for a process
+    // waiting for a message                                                                                                          
     int32 temp = receive();                                                                                                            
     printf("Ring Element %d : Round %d : Value : %d\n", curr, last , temp);                                                            
-    last += 1;                                                                                                                         
+    last += 1;      
+    // sending message to the neighbour process                                                                                                                   
     send(*next_pid, temp-1);                                                                                                           
-  }                                                                                                                                    
+  }    
+
+  // Passing messages back from the last process to the first which sends OK message to aprent so it stops execution.                                                                                                                                
   if(curr != pro_cre-1) {                                                                                                              
     receive();                                                                                                                         
     send(*prev_pid, OK);                                                                                                               
@@ -25,15 +29,19 @@ void message_process(pid32 *next_pid, int curr, pid32 *prev_pid, int32 count_val
 } 
 
 
+// Function to implement polling
 void polling_process(int *curr_ptr, int *next_ptr, int32 *count, int32 *total_rounds, int curr){
   int32 last = 0;
-  while (last < *(total_rounds)){
+  while (last < *(total_rounds)){// checking if the number of rounds have been completed for a process
     int32 temp = *(curr_ptr);
+    // polling operation done here, waits until a non -1 value appears inn the mailbox
     if(temp != -1){
       printf("Ring Element %d : Round %d : Value : %d\n", curr , last, temp);
       last = last + 1;
+      // incrementing count  for the parent to know when to stop execution
       *(count) = *(count) + 1;
       *(curr_ptr) = -1;
+      // passing message to the neighbour process.
       *(next_ptr) = temp -1;
     }
   }
--- shell/xsh_process_ring.c
+++ shell/xsh_process_ring.c
@@ -19,9 +19,8 @@ shellcmd xsh_process_ring(int argc, char *args[])
 {
   int32 p = 2; /* specified default */
   int32 r = 3; /* specified default */
-  char implementation[] = "poll";
+  char implementation[] = "poll"; /* specified default */
   char *pont_imp = implementation;
-  //char temp_value[];
   char *endp;
   char *endr;
   int32 i;
@@ -77,36 +76,36 @@ shellcmd xsh_process_ring(int argc, char *args[])
                   print_error();
                   printf("-r recieved invalid integer\n");
                   return SHELL_ERROR;
-              } 
+              }
+	       else if (!(0 <= r && r <= 100)) {
+                /* The number is out of range */
+                  print_error();
+                  printf("-r flag expected a number between 0 - 100\n");
+                  return SHELL_ERROR;
+              }
               printf("argument  is %d\n", r);}
 
 
       // Handling i parameter        
       else if (0 == strncmp("-i", args[i], 3)){
-
-                printf("Inside string sthingy\n");
+	// Checking whether the user has opted for polling or synchronization method
                 if(0 == strcmp(args[i+1],"poll")){
-          	// call poliing stuff here
-                  //temp_value = args[i+1];
                 pont_imp = args[i+1];
                 printf("The implementation type is %s\n", args[i+1]);}
                 else if(0 == strcmp(args[i+1],"sync")){
-
-                  //temp_value = args[i+1];
                   pont_imp = args[i+1];
-                  printf("The implementation type is %s\n", args[i+1]);
-          	// call message_passing here 
           }
           else {
     	print_error();
     	printf("Please provide a valid implementation option\n");
           }
         
+		// providing appropriate message for help parameter
         }else if (0 == strncmp("--h", args[i], 3)){
           
             printf("Example run: process_ring -p int_val -r uint32 -i string\n");
             printf("-p represents number of process beween 0 - 64, by default 2\n");
-            printf("-r represents number of rounds which is a uint32 value, by default 3\n");
+            printf("-r represents number of rounds which is between 0 - 100, by default 3\n");
             printf("-i represents implementation type, expecting either poll or sync, by default poll\n");
         
       }
@@ -114,54 +113,53 @@ shellcmd xsh_process_ring(int argc, char *args[])
         
         i += 1; /* Skip pass the numeric argument */
       }
-      printf("The Value for p is %d\n", p);
-      printf("The Value for r is %d\n", r);
-      printf("The Value for i is %s\n", pont_imp);
+
 
       if(0 == strcmp(pont_imp, "poll")){
-          printf("Correctly inside if block of poll\n");                              
-    /* This process has a lower priority than the parent process */                         
-    // int procs = 10;                                                                         
-    // int rounds = 2;                                                                         
-    // int total_number_pnt = (procs * rounds ) -1;  
-                  int total_number_pnt = (p * r) -1;                                          
-                  volatile int inbox_arr[p];                                                          
-                  volatile int32 count = -1;                                                              
-                  //volatile int count_arr[2];                                                            
+                  // Implementing the polling function 
+                  int total_number_pnt = (p * r) -1;  // total number to count down from                                        
+                  volatile int inbox_arr[p];  // declaring the mailbox array                                                        
+                  volatile int32 count = -1;     // count variable to check when the child processes all complete execution  
+
+                  // for loop to initialize the mailbox with -1 values (to help with polling)                                                                                                                
                   int q_val;                                                                                  
                   for(q_val =1; q_val< p; q_val++){                                                               
                     inbox_arr[q_val] = -1;                                                                    
                   }                                                                                       
-                  inbox_arr[0] = total_number_pnt;                                                        
-                  int y;                                                                                  
-                  //printf("count here %d\n", &count);                                                    
+                  inbox_arr[0] = total_number_pnt;     
+
+                  // Declaring the processes and resuming them to start.                                                   
+                  int y;                                                                                                                                   
                   for (y = 0; y < p-1 ; y++){                                                         
                     resume(create(polling_process, 1024, 20 , "val" , 5, &inbox_arr[y], &inbox_arr[y+1], &count, &r, y));                                                                              
                   }                                                                                       
                   resume(create(polling_process, 1024, 20 , "val" , 5, &inbox_arr[p-1], &inbox_arr[0], &count, &r, p-1));                                                                      
-                                                                                                          
-                  while(count < total_number_pnt);//{printf("count value %d \n",count);}                  
+                                
+                  // condition to check if child processes completed execution                                                                          
+                  while(count < total_number_pnt);               
                   return SHELL_OK;                                                                                                                                                                
       }  
       else if(0 == strcmp(pont_imp, "sync")){
+                  // getting parent process pid to pass message once the last child process completes execution.
                   pid32 parent_pid  = getpid();                                                                                                        
-                  // int procs = 10;                                                                                                                      
-                  // int rounds = 2;                                                                                                                      
-                  // int total_count = (procs * rounds) - 1;    
-                  int total_count = (p * r) - 1;                                                                                            
+                  int total_count = (p * r) - 1;   // total number to count down from                                                                                          
                   int index = 0;                                                                                                                               
-                  pid32 procs_array[p];                                                                                                            
+                  pid32 procs_array[p];   // getting an array of pid's to help message passing between processes easily
+
+                  // creating processes but in suspended state to collect pid's of all processes.                                                                                                         
                   procs_array[0] = create(message_process, 1024, 20, "consumer", 5, &procs_array[1], index, &parent_pid, r, p);                      
                   for(index = 1; index< p; index++){                                                                                                           
                     procs_array[index] = create(message_process, 1024, 20, "consumer", 5, &procs_array[(index+1)%p], index, &procs_array[index-1], r, p);    
                   }                                                                                                                                    
-                                                                                                                                                       
+                   
+                   // resuming the processes and starting execution of message passing.                                                                                                                                    
                   int j;                                                                                                                               
                   for(j = 0; j< p; j++){                                                                                                           
                     resume(procs_array[j]);                                                                                                            
                   }                                                                                                                                    
                   send(procs_array[0], total_count);                                                                                                   
-                                                                                                                                                       
+                  
+                  // condition to check if child processes completed execution                                                                                                                              
                   receive();                                                                                                                           
                   return SHELL_OK;                                                                                                                     
     }
@@ -169,6 +167,3 @@ shellcmd xsh_process_ring(int argc, char *args[])
     return SHELL_OK;
     }                
       
-
-    //   return SHELL_OK;
-    // }
